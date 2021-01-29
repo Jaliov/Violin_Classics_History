7d177877a0ee45617f176c7d06d768598db36963
Joel Rudin <jarud55@gmail.com>
2021-01-29 12:15:33 -0500

    test commit

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php echo "this is a test file" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Violin Classics History</title>
+</head>
+<body>
+    <h1>Learn about violinists here</h1>
+</body>
+</html>
