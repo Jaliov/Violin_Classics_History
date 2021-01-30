4dcab3dd2237b1e6d5b011a13bbccf30b4794b58
Joel Rudin <jarud55@gmail.com>
2021-01-29 22:15:35 -0500

    first steps

---
+++ video1.php
@@ -0,0 +1,50 @@
+<div class = "container">
+<?php include 'header.php'; ?>
+
+<?php include 'video_list.php'; ?>
+
+<?php
+
+
+echo "<div class='card' style='width: 100%;'>
+<div class='embed-responsive embed-responsive-21by9'>
+<iframe class='embed-responsive-item' src=$clip1></iframe>'
+</div><div class='card-body'>
+<h5 class='card-title'>Bach Concerto in A minor</h5>
+<p class='card-text'>Violinist: Kyung Wa Chung.</p>
+<a href='#' class='btn btn-primary'>Info about Bach</a>
+</div></div><br>";
+
+echo "<div class='embed-responsive embed-responsive-21by9'>
+<iframe class='embed-responsive-item' src=$clip5></iframe>'
+</div><br>";
+
+
+echo "<div class='embed-responsive embed-responsive-21by9'>
+<iframe class='embed-responsive-item' src=$clip2></iframe>'
+</div><br>";
+
+echo "<div class='embed-responsive embed-responsive-21by9'>
+<iframe class='embed-responsive-item' src=$clip3></iframe>'
+</div><br>";
+
+echo "<div class='embed-responsive embed-responsive-21by9'>
+<iframe class='embed-responsive-item' src=$clip4></iframe>'
+</div><br>";
+
+echo "<div class='embed-responsive embed-responsive-21by9'>
+<iframe class='embed-responsive-item' src=$clip8></iframe>'
+</div><br>";
+
+echo "<div class='embed-responsive embed-responsive-21by9'>
+<iframe class='embed-responsive-item' src=$clip7></iframe>'
+</div><br>";
+
+
+?>
+
+
+    </div>
+</body>
+</html>
+
---
+++ video_list.php
@@ -0,0 +1,20 @@
+<?php
+
+$clip1 = "https://www.youtube.com/embed/NllhcuOmiwM"; #Bach Kyung Wha Chung
+
+$clip2 =  "https://www.youtube.com/embed/tiGnVQPKHt0"; 
+
+$clip3 = "https://www.youtube.com/embed/cokCgWPRZPg?start=200;"; #Perlamn Beethoven Concerto
+
+$clip4 = "https://www.youtube.com/embed/K67o86CS5uo"; //Mutter Mendelssohn
+
+$clip5 = "https://www.youtube.com/embed/_ohsBg6onXY";  //Bach Double Jensen Kavakos
+
+$clip6 = "https://www.youtube.com/embed/RFkSiNp4CRQ"; // Brahms Oistrakh
+
+$clip7 = "https://www.youtube.com/embed/kFaq9kTlcaY"; //Heifetz Tchaikovsky
+
+$clip8 = "https://www.youtube.com/embed/wATRbVV51XU"; //Brahms Midori
+
+?>;
+
