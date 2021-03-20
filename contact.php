572ad89149badba177cdfe7de97c6557a2a5e901
Joel Rudin <jarud55@gmail.com>
2021-03-20 11:11:16 -0400

    test text

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
 require 'header.php'; 
-$message = "<p class = 'text-light'>process failed</p>";
+// $message = "<p class = 'text-light'>process failed</p>";
 
 //  if(isset ($_POST['name_entry'])) {
 
@@ -10,15 +10,15 @@ $message = "<p class = 'text-light'>process failed</p>";
 
 //     $email = $_POST['email'];
 
-$sql = 'INSERT INTO musicians(name_entry) VALUES( :nameEntry )';
+// $sql = 'INSERT INTO musicians(name_entry) VALUES( :nameEntry )';
 
    //  $stmt= $pdo->prepare($sql);
-$name = isset($_POST['name_entry']) ? $_POST['name_entry'] : null;
-$stmt = $connection->prepare($sql); 
-$stmt->execute( array( ':nameEntry'=>$name)); 
+// $name = isset($_POST['name_entry']) ? $_POST['name_entry'] : null;
+// $stmt = $connection->prepare($sql); 
+// $stmt->execute( array( ':nameEntry'=>$name)); 
 
 echo "<div class = 'container'>
-
+     <h1 class = 'text-danger'>Test Text</h1>
     <h2 class = 'text-light'>Enter Name</h2>
     
     <form method = 'POST' action = 'contact.php'>
