527241f06701b518e9287f61fce3181ba560b4f8
Joel Rudin <jarud55@gmail.com>
2021-03-20 11:24:24 -0400

    contact fix attempt

--- contact.php
+++ contact.php
@@ -1,24 +1,16 @@
 <?php 
 require 'header.php'; 
-// $message = "<p class = 'text-light'>process failed</p>";
+$message = "<p class = 'text-light'>process failed</p>";
 
-//  if(isset ($_POST['name_entry'])) {
-
-//     // echo 'submitted';
-    
-    
-
-//     $email = $_POST['email'];
-
-// $sql = 'INSERT INTO musicians(name_entry) VALUES( :nameEntry )';
+$sql = 'INSERT INTO musicians(name_entry) VALUES( :nameEntry )';
 
    //  $stmt= $pdo->prepare($sql);
-// $name = isset($_POST['name_entry']) ? $_POST['name_entry'] : null;
-// $stmt = $connection->prepare($sql); 
-// $stmt->execute( array( ':nameEntry'=>$name)); 
+$name = isset($_POST['name_entry']) ? $_POST['name_entry'] : $message;
+$statement = $connection->prepare($sql); 
+$statement->execute( array( ':nameEntry'=>$name)); 
 
 echo "<div class = 'container'>
-     <h1 class = 'text-danger'>Test Text</h1>
+
     <h2 class = 'text-light'>Enter Name</h2>
     
     <form method = 'POST' action = 'contact.php'>
@@ -36,5 +28,5 @@ echo "<div class = 'container'>
         </form>
 
         </div>" ?>
-        
+        <h1 class = 'danger'>Test</h1>
 <?php require 'footer.php' ?>
