ad5885fe5a7996605190325bcaaee6f73b94a0c5
Joel Rudin <jarud55@gmail.com>
2021-04-02 10:53:56 -0400

    Links page

--- links.php
+++ links.php
@@ -1,11 +1,18 @@
 <?php require 'header.php'; ?>
 
-<div style = "background-color: rgb(217, 83, 79, 0.2);box-shadow: 5px 10px rgb(217, 83, 79, 0.3);">
-<p class = 'alert text-white rounded-bottom'>This site was built by Joel Rudin, a professional violist, artist and web developer. Check out my other sites here:  </p> 
-    <ul class = 'text-light'>
-        <li>Eiusmod ad </li>
-        <li>E magna officia.</li>
-        <li>commodo sit</li>
+<div class="card text-danger" style="background-color: rgb(217, 83, 79, 0.2);">
+    <!-- <h5 class="card-header">Additional</h5> -->
+    <div class="card-body text-light text-center">
+        <p class="card-title">I am a professional musician as well as aritst and web developer
 
-</ul>
+        <p class="card-text">Check out some of my other sites here:</p>
+        <div class="btn-group-vertical"> 
+        <a href="https://boiling-beyond-86124.herokuapp.com/" class="btn btn-secondary">My Website Portfolio</a><br>
+        <a href="https://joelrudinart.com/" class="btn btn-dark">Artist Website</a><br>
+
+        <a href="https://lisabflamenco.com/index.html" class="btn btn-secondary">Lisa Botalico Flamenco!</a><br>
+        <a class="btn btn-dark" href="https://github.com/Jaliov/Code-Quiz-Viola">Viola Quiz!</a><br>
+    </div> 
 </div>
+</div>
+
