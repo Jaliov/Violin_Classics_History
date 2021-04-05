2aed1b2ee3ea90d6a8106248314a6a571f970fc1
Joel Rudin <jarud55@gmail.com>
2021-04-05 11:45:17 -0400

    contact revive attempt

--- contact.php
+++ contact.php
@@ -4,19 +4,32 @@ require 'header.php';
 $message = "failed";
 try {
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = 'INSERT INTO users(name_entry) VALUES( :nameEntry )';
+$sql = 'INSERT INTO users(name_entry, email, comments) VALUES( :nameEntry, :emailEntry, :textEntry )';
 
    //  $stmt= $pdo->prepare($sql);
 $name = isset($_POST['name_entry']) ? $_POST['name_entry'] : null;
 $stmt = $connection->prepare($sql); 
-$stmt->execute( array( ':nameEntry'=>$name)); 
+//  $stmt->execute( array( ':nameEntry'=>$name)); 
+
+$email = isset($_POST['email']) ? $_POST['email'] : null;
+$stmt = $connection->prepare($sql); 
+// $stmt->execute( array( ':emailEntry'=>$email)); 
+
+$comments = isset($_POST['comments']) ? $_POST['comments'] : null;
+$stmt = $connection->prepare($sql); 
+// $stmt->execute( array( ':textEntry'=>$comments)); 
+
+$stmt->execute( array( ':nameEntry'=>$name,  ':emailEntry'=>$email, ':textEntry'=>$comments)); 
 
 echo "<span class = 'text-light'>Submitted</span>";
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
 
-$connection = null;
+// $stmt = $connection->prepare($sql); 
+// $stmt->execute( array( ':nameEntry'=>$name,  ':emailEntry'=>$email, ':textEntry'=>$comments)); 
+
+// $connection = null;
 
 echo "<div class = 'container'>
     <h2 class = 'text-light'>Enter Name</h2>
@@ -26,11 +39,18 @@ echo "<div class = 'container'>
          
          <label for='name_entry'>Name</label>
          <input type='text' name = 'name_entry' class='form-control' placeholder = 'Enter name'>
-                 <button class ='btn btn-secondary' type = 'submit'>Enter Name</button>
-        
-        </div>
+
+                 <div class='form-group'>
+                 <label for='email'>Email address</label>
+                 <input type='email' class='form-control' name ='email' placeholder='name@sampleemail.com'> 
+               </div>
+               
+               <div class='form-group'>
+                 <label for='comments'>Comments</label>
+                 <textarea class='form-control' name='comments' rows='3' placeholder='Violin stuff...'></textarea>
+               <button class ='btn btn-secondary' type = 'submit' name='submit'>Submit</button>
+               </div>
         </form>
         </div>" ?>
-        
-<?php require 'footer.php' ?>
 
+<?php require 'footer.php' ?>
