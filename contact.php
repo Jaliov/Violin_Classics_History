9974d3ae61b029a370d6c901ae1248b7fa058a09
Joel Rudin <jarud55@gmail.com>
2021-04-05 09:05:42 -0400

    revert to users table

--- contact.php
+++ contact.php
@@ -4,14 +4,14 @@ require 'header.php';
 $message = "failed";
 try {
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = 'INSERT INTO musicians(name_entry) VALUES( :nameEntry )';
+$sql = 'INSERT INTO users(name_entry) VALUES( :nameEntry )';
 
    //  $stmt= $pdo->prepare($sql);
 $name = isset($_POST['name_entry']) ? $_POST['name_entry'] : null;
 $stmt = $connection->prepare($sql); 
 $stmt->execute( array( ':nameEntry'=>$name)); 
 
-echo "New record created successfully";
+echo "<span class = 'text-light'>Submitted</span>";
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
@@ -26,7 +26,7 @@ echo "<div class = 'container'>
          
          <label for='name_entry'>Name</label>
          <input type='text' name = 'name_entry' class='form-control' placeholder = 'Enter name'>
-                 <button class ='btn btn-info' type = 'submit'>Enter Name</button>
+                 <button class ='btn btn-secondary' type = 'submit'>Enter Name</button>
         
         </div>
         </form>
