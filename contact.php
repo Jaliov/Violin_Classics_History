712494c65aff3db8028f0238426a078b84ac7ede
Joel Rudin <jarud55@gmail.com>
2021-04-05 12:24:34 -0400

    contact form revive styling

--- contact.php
+++ contact.php
@@ -1,53 +1,50 @@
-<?php 
-require 'header.php'; 
 
+<?php 
+
+require 'header.php'; 
+$NameError="";
 $message = "failed";
 try {
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = 'INSERT INTO users(name_entry, email, comments) VALUES( :nameEntry, :emailEntry, :textEntry )';
 
-   //  $stmt= $pdo->prepare($sql);
 $name = isset($_POST['name_entry']) ? $_POST['name_entry'] : null;
+if(empty($_POST['name_entry'])) {
+  $NameError='Please submit a name!';
+}
 $stmt = $connection->prepare($sql); 
-//  $stmt->execute( array( ':nameEntry'=>$name)); 
 
 $email = isset($_POST['email']) ? $_POST['email'] : null;
 $stmt = $connection->prepare($sql); 
-// $stmt->execute( array( ':emailEntry'=>$email)); 
 
 $comments = isset($_POST['comments']) ? $_POST['comments'] : null;
 $stmt = $connection->prepare($sql); 
-// $stmt->execute( array( ':textEntry'=>$comments)); 
 
 $stmt->execute( array( ':nameEntry'=>$name,  ':emailEntry'=>$email, ':textEntry'=>$comments)); 
 
-echo "<span class = 'text-light'>Submitted</span>";
+
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
-
-// $stmt = $connection->prepare($sql); 
-// $stmt->execute( array( ':nameEntry'=>$name,  ':emailEntry'=>$email, ':textEntry'=>$comments)); 
-
-// $connection = null;
+$connection = null;
 
 echo "<div class = 'container'>
-    <h2 class = 'text-light'>Enter Name</h2>
-    
+<style> input, #textArea {
+  box-shadow: 5px 10px rgb(217, 83, 79, 0.3)} </style>   
     <form method = 'POST' action = 'contact.php'>
-       <div class='form-group'>
-         
-         <label for='name_entry'>Name</label>
-         <input type='text' name = 'name_entry' class='form-control' placeholder = 'Enter name'>
+          
+         <label for='name_entry' class ='text-light'>Name</label>
+         <input type='text' name = 'name_entry' class='form-control' placeholder = 'Enter name'><span class = 'text-danger'>
+         $NameError </span>
 
                  <div class='form-group'>
-                 <label for='email'>Email address</label>
+                 <label for='email' class ='text-light'><small>Email</small></label>
                  <input type='email' class='form-control' name ='email' placeholder='name@sampleemail.com'> 
                </div>
                
                <div class='form-group'>
-                 <label for='comments'>Comments</label>
-                 <textarea class='form-control' name='comments' rows='3' placeholder='Violin stuff...'></textarea>
+                 <label for='comments' class ='text-light'>Comments</label>
+                 <textarea class='form-control' name='comments' rows='3' placeholder='Violin stuff...' id='textArea'></textarea><br>
                <button class ='btn btn-secondary' type = 'submit' name='submit'>Submit</button>
                </div>
         </form>
