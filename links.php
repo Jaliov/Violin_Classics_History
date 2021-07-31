b94bfc88e2066b573f59197e8b612fa10033d75c
Joel Rudin <jarud55@gmail.com>
2021-07-31 14:30:35 -0400

    text changes

--- links.php
+++ links.php
@@ -1,23 +1,28 @@
-
 <?php require 'header-min.php'; ?>
 <style>
-<?php include 'style-min.css'; ?>
+<?php include 'style-min.css';
+?>
 </style>
 
 <div class="card text-danger bg-transparent">
     <!-- <h5 class="card-header">Additional</h5> -->
     <div class="card-body text-light text-center">
-        <div class ="card-title">I am a musician, artist and web developer</div>
+        <div class="card-title">I am a musician, artist and web developer</div>
 
-        <div class="card-text btn-list">Check out a few of my other sites here:</div><br>
-        <div class="text-center"> 
-            <a href="https://boiling-beyond-86124.herokuapp.com/" class="btn btn-outline-danger btn-light' btn-list" target="_blank">Website
-                Portfolio</a><br>
-            <a href="https://jaliov.github.io/JoelRudinArt/index.html" class="btn btn-outline-danger btn-light btn-list" target="_blank">Artist
+        <div class="card-text btn-list">Check out some of my other sites here:</div><br>
+        <div class="text-center">
+
+            <a href="https://jaliov.github.io/JoelRudinArt/index.html" class="btn btn-outline-danger btn-light btn-list"
+                target="_blank">Artist
                 Website</a><br>
-            <a href="https://lisabflamenco.herokuapp.com/" class="btn btn-outline-danger btn-light btn-list" target="_blank">Lisa Botalico
+            <a href="https://lisabflamenco.herokuapp.com/" class="btn btn-outline-danger btn-light btn-list"
+                target="_blank">Lisa Botalico
                 Flamenco!</a><br>
-            <a class="btn btn-outline-danger btn-light btn-list" href="https://frozen-beach-43331.herokuapp.com/" target="_blank">Viola Quiz!</a>
+            <a class="btn btn-outline-danger btn-light btn-list" href="https://frozen-beach-43331.herokuapp.com/"
+                target="_blank">Viola Quiz!</a>
+            <a href="https://boiling-beyond-86124.herokuapp.com/" class="btn btn-outline-danger btn-light' btn-list"
+                target="_blank">Website
+                Portfolio</a><br>
         </div>
     </div>
 </div>
