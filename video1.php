dc4946a0b8690bb294836d4010a7c368e1d35bbb
Joel Rudin <jarud55@gmail.com>
2021-08-01 11:31:41 -0400

    viola link edit

--- video1.php
+++ video1.php
@@ -121,9 +121,8 @@ $stmt->execute(array($id)); $musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
 ?>
 
 <?php foreach($musicians as $musician): ?>
-<?php echo $buttonPlace . "data-content='$musician->info'  title = '$musician->role'>$musician->name</button><br/><a class = 'btn btn-outline-danger btn-sm text-light' href='https://frozen-beach-43331.herokuapp.com/' target='_blank'   id='heifetz_link'>Test your basic knowledge of the viola with my viola quizz!</a>"; ?>
+<?php echo $buttonPlace . "data-content='$musician->info'  title = '$musician->role'>$musician->name</button><br/><a class = 'btn btn-outline-danger btn-sm text-light' href='https://frozen-beach-43331.herokuapp.com/' target='_blank'   id='heifetz_link'>Test your knowledge of the viola here!</a>"; ?>
 <?php endforeach; echo "</div></div><br>";?>
-
 
 <?php echo $view5; ?>
 <!--Beethoven -->
